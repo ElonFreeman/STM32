d4bdcde731d00e0525a00657894afde9e64532e4
ElonFreeman <zhanghao112358@outlook.com>
2026-09-05 20:35:12 +0800

    26090502

--- oscilloscope_and_logicanalizer_test/build/Debug/CMakeFiles/4.3.1/CMakeSystem.cmake
+++ oscilloscope_and_logicanalizer_test/build/Debug/CMakeFiles/4.3.1/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-7.1.12-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM "Linux-7.1.13-200.fc44.x86_64")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "7.1.12-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM_VERSION "7.1.13-200.fc44.x86_64")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/fedora/STM32_projects/oscilloscope_and_logicanalizer_test/cmake/gcc-arm-none-eabi.cmake")
